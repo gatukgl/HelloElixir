4ec9b182a54cf484e6e434263e2cea592c8a3b9f
Gatuk Chattanon <gatukgl@gmail.com>
2021-08-02 22:01:26 +0700

    Add minimal todo to repository

---
+++ minimal_todo/minimal_todo.ex
@@ -0,0 +1,9 @@
+defmodule MinimalTodo do
+   def start do
+     # ask user for file name
+     # open the file and read it
+     # parse the data
+     # ask user for command
+     # (read todos, add todos, delete todos, load file, save file)  
+   end 
+end
