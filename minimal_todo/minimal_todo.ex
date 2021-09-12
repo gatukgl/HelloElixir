7f2f1fd293f6b969136e17873f4c4c1cb7cb9bac
Gatuk Chattanon <gatukgl@gmail.com>
2021-09-12 18:14:23 +0700

    Add minimal todo file with starter todo csv file

--- minimal_todo/minimal_todo.ex
+++ minimal_todo/minimal_todo.ex
@@ -1,9 +1,21 @@
 defmodule MinimalTodo do
-   def start do
-     # ask user for file name
-     # open the file and read it
-     # parse the data
-     # ask user for command
-     # (read todos, add todos, delete todos, load file, save file)  
-   end 
+  def start do
+    # ask user for file name
+    # open file and read
+    # parse the data
+    # ask user for command
+    # (read todos, add todos, delete todos, load file, save files)
+    filename = IO.gets("Name of .csv to load:")
+               |> String.trim
+    read(filename)
+  end
+
+  def read(filename) do
+    case File.read(filename) do
+      {:ok, body} -> body
+      {:error, reason} ->
+        IO.puts(~s(Could not open file "#{filename}"))
+        IO.inspect(reason)
+    end
+  end
 end
